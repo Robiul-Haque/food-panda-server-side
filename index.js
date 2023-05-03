fbe1adc64b044474b820dd9be2b334c78554a1da
Robiul-Haque <robiulcoc420@gmail.com>
2023-05-03 10:57:06 +0600

    create chef json data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+const chef = require('./data/chef.json');
 
 app.get('/', (req, res) => {
     res.send('express is running')
@@ -8,4 +9,10 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
+})
+
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
+    // http://localhost:5000/chef
 })
