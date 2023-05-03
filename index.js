3ee487bb3462cacf5e402c57994d72e52300fdce
Robiul-Haque <robiulcoc420@gmail.com>
2023-05-03 11:55:16 +0600

    install cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
-const chef = require('./data/chef.json');
+const chef = require('./data/chef.json')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('express is running')
